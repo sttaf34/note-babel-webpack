b0c3cac8abb69bdbf13c7361fbb5b4c4fb07fa59
Shinichi Kawamura <sttaf34@gmail.com>
2020-02-18 21:07:00 +0900

    とにかく必要最小限な構成で babel を使ってみた

---
+++ hello-babel/src/index.js
@@ -0,0 +1,18 @@
+const functionA = () => {
+  console.log("Hello World!")
+}
+
+class classA {
+  constructor(name) {
+    this.name;
+  }
+
+  say() {
+    console.log(this.name)
+  }
+}
+
+functionA()
+
+const user = new ClassA("sttaf34")
+console.log(user)
