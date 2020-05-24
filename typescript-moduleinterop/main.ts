0d736e42ad82fae6da7bd111c7e049c8027bb375
Shinichi Kawamura <sttaf34@gmail.com>
2020-05-24 20:02:19 +0900

    esModuleInterop のメモ追加

---
+++ typescript-moduleinterop/main.ts
@@ -0,0 +1,10 @@
+// "esModuleInterop": true,
+import moment from "moment"
+
+// "esModuleInterop": false
+// import * as moment from "moment"
+
+console.log(moment())
+
+// 詳しい
+// https://osamtimizer.hatenablog.com/entry/2018/06/28/122502
