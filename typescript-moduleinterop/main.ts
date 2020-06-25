e2ccce1ff028c3b491103d548e3892076ecf6293
Shinichi Kawamura <sttaf34@gmail.com>
2020-06-25 15:11:47 +0900

    moduleinterop のメモ追加

--- typescript-moduleinterop/main.ts
+++ typescript-moduleinterop/main.ts
@@ -1,10 +1,36 @@
-// "esModuleInterop": true,
-import moment from "moment"
+// esModuleInterop について詳しく書いてあってありがたい
+// https://osamtimizer.hatenablog.com/entry/2018/06/28/122502
 
-// "esModuleInterop": false
-// import * as moment from "moment"
+// "esModuleInterop": true,
+// /*
+
+import moment from "moment"
+import dayjs from "dayjs"
+import parse from "csv-parse/lib/sync"
+import { readFileSync } from "fs"
 
 console.log(moment())
+console.log(dayjs().locale())
 
-// 詳しい
-// https://osamtimizer.hatenablog.com/entry/2018/06/28/122502
+const data = readFileSync("./sample.csv")
+const fruitData = parse(data, { columns: true })
+console.log(fruitData)
+
+// */
+
+// "esModuleInterop": false
+/*
+
+import * as moment from "moment"
+import * as dayjs from "dayjs"
+import * as parse from "csv-parse/lib/sync"
+import { readFileSync } from "fs"
+
+console.log(moment())
+console.log(dayjs().locale())
+
+const data = readFileSync("./sample.csv")
+const fruitData = parse(data, { columns: true })
+console.log(fruitData)
+
+// */
