fe8e6bbc978786f58c02bd3fd1bc908e42923318
Shinichi Kawamura <sttaf34@gmail.com>
2020-07-24 00:44:24 +0900

    Expo のプロジェクト追加

---
+++ hello-expo/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { HelloView } from "./src/screens/HelloView"
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <HelloView />
+    </>
+  )
+}
+
+// eslint-disable-next-line import/no-default-export
+export default App
